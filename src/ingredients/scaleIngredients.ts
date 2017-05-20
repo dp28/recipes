691c4d993e866bcb963bcbf2beb831aee62ec561
Daniel Patterson <dan24patt@gmail.com>
2017-05-20 14:30:51 +0100

    Scale multiple ingredients by changing one

--- src/ingredients/scaleIngredients.ts
+++ src/ingredients/scaleIngredients.ts
@@ -1,4 +1,4 @@
-import { scale } from '../measurement';
+import { scale, Measurement } from '../measurement';
 import { Ingredients, IngredientMap, toIngredientMap, buildIngredientMap } from './measuredIngredients';
 import { MeasuredIngredient } from './ingredient';
 
@@ -7,15 +7,33 @@ export function scaleIngredientsByFactor(factor: number, ingredients: Ingredient
   return mapValues(scaleIngredient(factor), ingredientMap);
 }
 
-function mapValues<T, U>(func: (t: T) => U, object: { [_:string]: T }): { [_:string]: U } {
-  return Object
-    .keys(object)
-    .reduce((mapped: { [_:string]: U }, key) => {
-      mapped[key] = func(object[key]);
-      return mapped;
-    }, {});
+export function scaleIngredientsByIngredient(
+  ingredient: MeasuredIngredient,
+  ingredients: Ingredients
+): IngredientMap {
+  const ingredientMap = toIngredientMap(ingredients);
+  const oldIngredient = ingredientMap[ingredient.substance];
+  if (oldIngredient) {
+    const ratio = calculateRatioBetween(ingredient.measurement, oldIngredient.measurement);
+    return scaleIngredientsByFactor(ratio, ingredientMap);
+  }
+  return ingredientMap;
+}
+
+function calculateRatioBetween(first: Measurement, second: Measurement): number {
+  if (first.unit !== second.unit) {
+    throw 'Conversion between units during scaling is not supported';
+  }
+  return first.value / second.value;
 }
 
 function scaleIngredient(factor: number): (ingredient: MeasuredIngredient) => MeasuredIngredient {
   return ingredient => ({ ...ingredient, measurement: scale(ingredient.measurement, factor) });
+}
+
+function mapValues<T, U>(func: (t: T) => U, object: { [_:string]: T }): { [_:string]: U } {
+  return Object.keys(object).reduce((mapped: { [_:string]: U }, key) => {
+      mapped[key] = func(object[key]);
+      return mapped;
+    }, {});
 }
